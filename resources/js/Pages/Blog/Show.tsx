a0b35b4a3e43ee5178d6f33ce4e0cccc4cc74c0f
xCharlotte <charlottevoskuilen@hotmail.com>
2025-06-29 14:43:44 +0200

    IMP: styling css

--- resources/js/Pages/Blog/Show.tsx
+++ resources/js/Pages/Blog/Show.tsx
@@ -17,24 +17,26 @@ export type BlogData = {
 
 export default function Show({ blog }: BlogData) {
   return (
-    <>
-      <div className="flex flex-col min-h-screen">
-        <Head title={blog.title} />
-        <Navbar />
-        <Hero
-          title={blog.title}
-          imageUrl={blog.image}
-          imagePositionClass="lg:bg-[center_top_-16rem]"
-        />
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-2xl font-bold mb-4">{blog.title}</h1>
-          <p className="text-gray-700">
-            <div dangerouslySetInnerHTML={{ __html: blog.content }} />
-          </p>
+    <div className="flex flex-col min-h-screen">
+      <Head title={blog.title} />
+      <Navbar />
+      <Hero
+        title={blog.title}
+        imageUrl={blog.image}
+        imagePositionClass="lg:bg-[center_top_-16rem]"
+      />
+      <section className="py-8 md:py-14 lg:py-28 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-5xl mx-auto flex flex-col gap-12">
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold mb-4">{blog.title}</h1>
+            <p className="text-gray-700">
+              <div dangerouslySetInnerHTML={{ __html: blog.content }} />
+            </p>
+          </div>
         </div>
-      </div>
+      </section>
       <GalleryDivider />
       <Footer />
-    </>
+    </div>
   );
 }
